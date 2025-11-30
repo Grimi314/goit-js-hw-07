1f20ef7a9ea1e617463d1479255c078dcf292d51
Maryan Serhii <sergijmara210@gmail.com>
2025-12-01 00:14:38 +0200

    fix message in console log

--- js/task-1.js
+++ js/task-1.js
@@ -2,12 +2,12 @@
 const list = document.querySelector("#categories");
 const item = list.querySelectorAll(".item");
 
-console.log("Number of categories", item.length);
+console.log("Number of categories:", item.length);
 
 item.forEach((item) => {
   const title = item.querySelector("h2").textContent;
   const element = item.querySelectorAll("ul li").length;
 
-  console.log("Caregries", title);
-  console.log("Element", element);
+  console.log("Category:", title);
+  console.log("Elements:", element);
 });
